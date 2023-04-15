8302889bac59d58f695cf795647f7c335939d1e8
Alpha1Dash1 <flalpha1dash1@gmail.com>
2023-04-15 20:13:45 +0500

    HW 1

---
+++ addForm.js
@@ -0,0 +1,43 @@
+addBtn.addEventListener("click", e => {
+	mdBox.style.display = "flex";
+})
+mdClose.addEventListener("click", e => {
+	mdBox.style = null;
+})
+
+addForm.addEventListener("submit", e => {
+	e.preventDefault();
+	const body = {};
+	
+	for (let i=0; i<addForm.elements.length; i++) {
+		const inp = addForm.elements[i];
+		if (inp.name) {
+			if (inp.type === "checkbox") {
+				body[inp.name]=inp.checked;
+			} else {
+				body[inp.name]=inp.value;
+			}
+		}
+	}
+	fetch(path+"/add", {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(body)
+	})
+	.then(res => {
+		if(res.ok) {
+			addForm.reset();
+			mdBox.style = null;
+			createCard(body);
+		} else {
+			return res.text();
+		}
+	})
+	.then(err => {
+		if(err && err.message) {
+			alert(err.message);
+		};
+	});
+})
---
+++ global.js
@@ -0,0 +1,8 @@
+const box = document.querySelector(".container");
+const addBtn = document.querySelector(".add-btn");
+const mdBox = document.querySelector(".modal-container");
+const mdClose = document.querySelector(".modal-close");
+const addForm =document.forms.add;
+
+const user = "Alpha1Dash1";
+const path = `https://cats.petiteweb.dev/api/single/${user}`;
